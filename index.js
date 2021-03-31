711a0cc2a21f707f78d4495a38e0a432dafa36ad
Kulakov Pavel <kombojiec@gmail.com>
2021-03-31 13:51:13 +0300

    feat(recommended): add recommend section

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-$( document ).ready(function() {
-  $('[data-fancybox="gallery"]').fancybox({
-    autoPlay : true,
-    loop: true,
-    keyboard: false,
-    toolbar: false,
-    modal: false,
-    slideShow: {
-      autoStart: true,
-      speed: 3000
-    },
-    hideScrollbar: true,
-    autoFocus: true,
-    wheel: false,
-  });
+document.addEventListener('DOMContentLoaded', function(){ 
+  "use strict";
+
+
 });
